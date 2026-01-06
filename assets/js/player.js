c6c7c80df8625f2bf60e2408da03dc8f1e2f4082
mediaxlus <164855933+mediaxlus@users.noreply.github.com>
2026-01-07 05:48:39 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,1 +1,20 @@
+const video = document.getElementById('player');
+let unlocked = false;
+
+export function loadVideo(src, poster) {
+  video.src = src;
+  video.poster = poster;
+  video.currentTime = 0;
+}
+
+video.addEventListener('timeupdate', () => {
+  if (!unlocked && video.currentTime >= APP.previewLimit) {
+    video.pause();
+    showUnlock();
+  }
+});
+
+function showUnlock() {
+  alert('Preview selesai. Login untuk lanjut HD.');
+}
 
