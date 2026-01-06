bec939c711e375f6545766628399f45671354d15
mediaxlus <164855933+mediaxlus@users.noreply.github.com>
2026-01-07 05:47:49 +0700

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,10 @@
+async function fetchMovie(id, lang='id-ID') {
+  const key = `${id}_${lang}`;
+  if (APP.cache.has(key)) return APP.cache.get(key);
+
+  const res = await fetch(`${APP.api}?id=${id}&lang=${lang}`);
+  const data = await res.json();
+  APP.cache.set(key, data);
+  return data;
+}
 
