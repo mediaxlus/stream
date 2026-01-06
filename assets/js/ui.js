7a8bd72e8d71cfed06c9245f212a025f77d270ec
mediaxlus <164855933+mediaxlus@users.noreply.github.com>
2026-01-07 05:49:32 +0700

    Update ui.js

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,1 +1,12 @@
+async function renderMovie(id) {
+  const m = await fetchMovie(id);
+  title.textContent = m.title;
+  meta.textContent = `${m.release_date} • ⭐ ${m.vote_average}`;
+  overview.textContent = m.overview;
+
+  loadVideo(
+    '/media/sample.mp4',
+    APP.imageBase + m.backdrop_path
+  );
+}
 
